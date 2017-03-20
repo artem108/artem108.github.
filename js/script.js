daf86b23c78a3fb07620e1acf952caac350e1977
Artem Arshaniy <artem.arshaniy108@gmail.com>
2017-03-20 19:36:57 +0200

    add img

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
 
+function scroll() {
+
   var aboutBtn = document.getElementById('about');
   var uslugiBtn = document.getElementById('uslugi');
   var priceBtn = document.getElementById('price');
- var contactBtn = document.getElementById('contact');
- function scroll() {
+  var contactBtn = document.getElementById('contact');
 
-
-    aboutBtn.onclick =  function () {
-      scrollBy(0, 800);
-    };
-
-    uslugiBtn.onclick = function () {
-      scrollBy(0, 1250);
-    };
-
-    priceBtn.onclick = function () {
-      scrollBy(0, 1620);
-    };
-
-    contactBtn.onclick = function () {
-      scrollBy(0, 1900);
-    };
-
-  }
+  aboutBtn.onclick =  function () {
+    scrollBy(0, 800);
+  };
+  uslugiBtn.onclick = function () {
+    scrollBy(0, 1250);
+  };
+  priceBtn.onclick = function () {
+    scrollBy(0, 1620);
+  };
+  contactBtn.onclick = function () {
+    scrollBy(0, 1900);
+  };
+}
 scroll();
